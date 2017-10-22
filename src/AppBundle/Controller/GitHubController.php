f41f2cbc52237e2bdd8b2501ed570020e346f78c
Rakesh Pal <rakeshpal24@gmail.com>
2017-10-23 01:28:56 +0530

    first commit

---
+++ src/AppBundle/Controller/GitHubController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Github\Client;
+
+class GitHubController extends Controller
+{
+    /**
+     * @Route("/list", name="listdir")
+     */
+    public function indexAction(Request $request)
+    {
+        
+        $client = new Client();
+
+        //$repos = $client->api('repo')->all();
+
+
+        $repos = $client->api('repo')->find('symfony');
+
+        $repositories = $repos['repositories'];
+
+        $symfony_repos = array();
+
+        foreach($repositories as $repository){
+            if (strpos($repository['url'], 'https://github.com/symfony/') !== false) {
+                
+                $symfony_repos[] = $repository;
+                
+            }
+        }
+
+        return $this->render('github/index.html.twig', [
+                'repos' => $symfony_repos,
+            ]);
+
+        //echo "<pre>"; print_r($symfony_repos); die('end');
+
+    }
+}
